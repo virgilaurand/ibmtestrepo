607be2fdaa04fe045fe0c984f6f9a3616408a479
Virgil Aurand <10892467+virgilaurand@users.noreply.github.com>
2021-02-10 09:26:17 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+o the API, the API translates
+the text and sends the translation back to you. In this case we translate English to
+Spanish. In this video, we’ve discussed what an API is, API Libraries, REST APIs,
+including Request and Response. Thank you for watching this video.
